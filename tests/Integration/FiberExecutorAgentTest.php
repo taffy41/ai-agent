chr-hertel/ai

[Agent] Add tool execution strategies for bulk execution
Introduce a pluggable execution model for the tool calls a model requests.
`FiberToolExecutor` implements the existing `ToolExecutorInterface` and runs
all tool calls as PHP Fibers, starting every fiber before collecting results
and scheduling suspended fibers round-robin so I/O-bound tools can interleave
their setup work. Results are returned in the order of the tool calls, keeping
the executor a drop-in replacement for the default `SequentialToolExecutor`.

The `SuspendableTrait` gives tools a `$this->suspend()` yield point that is a
no-op outside a fiber, so tools stay testable under any executor.

The AI bundle exposes this per agent through the `tools.execution_strategy`
option ("sequential", "fiber", or a custom service ID).

src/agent/tests/Integration/FiberExecutorAgentTest.php
@@ -1,0 +1,175 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Agent\Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\AI\Agent\Agent;
+use Symfony\AI\Agent\Bridge\Wikipedia\Wikipedia;
+use Symfony\AI\Agent\Toolbox\FiberToolExecutor;
+use Symfony\AI\Agent\Toolbox\Source\SourceCollection;
+use Symfony\AI\Agent\Toolbox\Toolbox;
+use Symfony\AI\Platform\Message\Message;
+use Symfony\AI\Platform\Message\MessageBag;
+use Symfony\AI\Platform\Result\TextResult;
+use Symfony\AI\Platform\Result\ToolCall;
+use Symfony\AI\Platform\Result\ToolCallResult;
+use Symfony\AI\Platform\Test\InMemoryPlatform;
+use Symfony\AI\Platform\TokenUsage\TokenUsage;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\JsonMockResponse;
+
+/**
+ * @author Denis Zunke <denis.zunke@gmail.com>
+ */
+final class FiberExecutorAgentTest extends TestCase
+{
+    public function testAgentCompletesWithFiberExecutorAndSingleToolCall()
+    {
+        $wikipedia = new Wikipedia(new MockHttpClient([
+            new JsonMockResponse([
+                'query' => [
+                    'search' => [
+                        ['title' => 'Symfony'],
+                    ],
+                ],
+            ]),
+        ]));
+
+        $toolbox = new Toolbox([$wikipedia]);
+
+        $invocationCount = 0;
+        $platform = new InMemoryPlatform(static function ($model, $input, $options) use (&$invocationCount) {
+            ++$invocationCount;
+
+            return match ($invocationCount) {
+                1 => new ToolCallResult([new ToolCall('call_wiki_1', 'wikipedia_search', ['query' => 'Symfony PHP'])]),
+                2 => new TextResult('Symfony is a PHP web framework.'),
+                default => new TextResult('Unexpected call'),
+            };
+        });
+
+        $agent = new Agent($platform, 'gpt-4', toolbox: $toolbox, toolExecutor: new FiberToolExecutor($toolbox));
+
+        $messages = new MessageBag(
+            Message::ofUser('What is Symfony?'),
+        );
+
+        $result = $agent->call($messages);
+
+        $this->assertInstanceOf(TextResult::class, $result);
+        $this->assertSame('Symfony is a PHP web framework.', $result->getContent());
+        $this->assertSame(2, $invocationCount);
+    }
+
+    public function testAgentCollectsSourcesAcrossParallelToolCallsWithFiberExecutor()
+    {
+        $wikipedia = new Wikipedia(new MockHttpClient([
+            new JsonMockResponse([
+                'query' => [
+                    'pages' => [
+                        '1' => [
+                            'title' => 'Symfony',
+                            'extract' => 'Symfony is a PHP framework.',
+                        ],
+                    ],
+                ],
+            ]),
+            new JsonMockResponse([
+                'query' => [
+                    'pages' => [
+                        '2' => [
+                            'title' => 'Laravel',
+                            'extract' => 'Laravel is another PHP framework.',
+                        ],
+                    ],
+                ],
+            ]),
+        ]));
+
+        $toolbox = new Toolbox([$wikipedia]);
+
+        $toolCallSymfony = new ToolCall('call_symfony', 'wikipedia_article', ['title' => 'Symfony']);
+        $toolCallLaravel = new ToolCall('call_laravel', 'wikipedia_article', ['title' => 'Laravel']);
+
+        $invocationCount = 0;
+        $platform = new InMemoryPlatform(static function ($model, $input, $options) use (&$invocationCount, $toolCallSymfony, $toolCallLaravel) {
+            ++$invocationCount;
+
+            return match ($invocationCount) {
+                1 => new ToolCallResult([$toolCallSymfony, $toolCallLaravel]),
+                2 => new TextResult('Symfony and Laravel are both PHP frameworks.'),
+                default => new TextResult('Unexpected call'),
+            };
+        });
+
+        $agent = new Agent($platform, 'gpt-4', toolbox: $toolbox, toolExecutor: new FiberToolExecutor($toolbox), includeSources: true);
+
+        $messages = new MessageBag(
+            Message::ofUser('Compare Symfony and Laravel.'),
+        );
+
+        $result = $agent->call($messages);
+
+        $this->assertInstanceOf(TextResult::class, $result);
+        $this->assertSame(2, $invocationCount);
+
+        $metadata = $result->getMetadata();
+        $this->assertTrue($metadata->has('sources'));
+        $this->assertInstanceOf(SourceCollection::class, $sources = $metadata->get('sources'));
+        $this->assertCount(2, $sources);
+    }
+
+    public function testAgentAggregatesTokenUsageWithFiberExecutor()
+    {
+        $wikipedia = new Wikipedia(new MockHttpClient([
+            new JsonMockResponse([
+                'query' => [
+                    'search' => [['title' => 'PHP']],
+                ],
+            ]),
+        ]));
+
+        $toolbox = new Toolbox([$wikipedia]);
+
+        $invocationCount = 0;
+        $platform = new InMemoryPlatform(static function ($model, $input, $options) use (&$invocationCount) {
+            ++$invocationCount;
+
+            if (1 === $invocationCount) {
+                $result = new ToolCallResult([new ToolCall('call_php', 'wikipedia_search', ['query' => 'PHP programming'])]);
+                $result->getMetadata()->add('token_usage', new TokenUsage(promptTokens: 100, completionTokens: 20, totalTokens: 120));
+
+                return $result;
+            }
+
+            $result = new TextResult('PHP is a server-side scripting language.');
+            $result->getMetadata()->add('token_usage', new TokenUsage(promptTokens: 150, completionTokens: 30, totalTokens: 180));
+
+            return $result;
+        });
+
+        $agent = new Agent($platform, 'gpt-4', toolbox: $toolbox, toolExecutor: new FiberToolExecutor($toolbox));
+
+        $messages = new MessageBag(
+            Message::ofUser('Tell me about PHP.'),
+        );
+
+        $result = $agent->call($messages);
+
+        $this->assertInstanceOf(TextResult::class, $result);
+        $this->assertSame(2, $invocationCount);
+        // In non-streaming mode the final result's token_usage is a plain TokenUsage.
+        $tokenUsage = $result->getMetadata()->get('token_usage');
+        $this->assertInstanceOf(TokenUsage::class, $tokenUsage);
+        $this->assertSame(180, $tokenUsage->getTotalTokens());
+    }
+}

taffy41/ai-agent

tests/Integration/FiberExecutorAgentTest.php
@@ -1,0 +1,177 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Agent\Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\AI\Agent\Agent;
+use Symfony\AI\Agent\Bridge\Wikipedia\Wikipedia;
+use Symfony\AI\Agent\Toolbox\FiberToolExecutor;
+use Symfony\AI\Agent\Toolbox\Source\SourceCollection;
+use Symfony\AI\Agent\Toolbox\Toolbox;
+use Symfony\AI\Platform\Message\Message;
+use Symfony\AI\Platform\Message\MessageBag;
+use Symfony\AI\Platform\Result\TextResult;
+use Symfony\AI\Platform\Result\ToolCall;
+use Symfony\AI\Platform\Result\ToolCallResult;
+use Symfony\AI\Platform\Test\InMemoryPlatform;
+use Symfony\AI\Platform\TokenUsage\TokenUsage;
+use Symfony\AI\Platform\TokenUsage\TokenUsageAggregation;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\JsonMockResponse;
+
+/**
+ * @author Denis Zunke <denis.zunke@gmail.com>
+ */
+final class FiberExecutorAgentTest extends TestCase
+{
+    public function testAgentCompletesWithFiberExecutorAndSingleToolCall()
+    {
+        $wikipedia = new Wikipedia(new MockHttpClient([
+            new JsonMockResponse([
+                'query' => [
+                    'search' => [
+                        ['title' => 'Symfony'],
+                    ],
+                ],
+            ]),
+        ]));
+
+        $toolbox = new Toolbox([$wikipedia]);
+
+        $invocationCount = 0;
+        $platform = new InMemoryPlatform(static function ($model, $input, $options) use (&$invocationCount) {
+            ++$invocationCount;
+
+            return match ($invocationCount) {
+                1 => new ToolCallResult([new ToolCall('call_wiki_1', 'wikipedia_search', ['query' => 'Symfony PHP'])]),
+                2 => new TextResult('Symfony is a PHP web framework.'),
+                default => new TextResult('Unexpected call'),
+            };
+        });
+
+        $agent = new Agent($platform, 'gpt-4', toolbox: $toolbox, toolExecutor: new FiberToolExecutor($toolbox));
+
+        $messages = new MessageBag(
+            Message::ofUser('What is Symfony?'),
+        );
+
+        $result = $agent->call($messages)->getResult();
+
+        $this->assertInstanceOf(TextResult::class, $result);
+        $this->assertSame('Symfony is a PHP web framework.', $result->getContent());
+        $this->assertSame(2, $invocationCount);
+    }
+
+    public function testAgentCollectsSourcesAcrossParallelToolCallsWithFiberExecutor()
+    {
+        $wikipedia = new Wikipedia(new MockHttpClient([
+            new JsonMockResponse([
+                'query' => [
+                    'pages' => [
+                        '1' => [
+                            'title' => 'Symfony',
+                            'extract' => 'Symfony is a PHP framework.',
+                        ],
+                    ],
+                ],
+            ]),
+            new JsonMockResponse([
+                'query' => [
+                    'pages' => [
+                        '2' => [
+                            'title' => 'Laravel',
+                            'extract' => 'Laravel is another PHP framework.',
+                        ],
+                    ],
+                ],
+            ]),
+        ]));
+
+        $toolbox = new Toolbox([$wikipedia]);
+
+        $toolCallSymfony = new ToolCall('call_symfony', 'wikipedia_article', ['title' => 'Symfony']);
+        $toolCallLaravel = new ToolCall('call_laravel', 'wikipedia_article', ['title' => 'Laravel']);
+
+        $invocationCount = 0;
+        $platform = new InMemoryPlatform(static function ($model, $input, $options) use (&$invocationCount, $toolCallSymfony, $toolCallLaravel) {
+            ++$invocationCount;
+
+            return match ($invocationCount) {
+                1 => new ToolCallResult([$toolCallSymfony, $toolCallLaravel]),
+                2 => new TextResult('Symfony and Laravel are both PHP frameworks.'),
+                default => new TextResult('Unexpected call'),
+            };
+        });
+
+        $agent = new Agent($platform, 'gpt-4', toolbox: $toolbox, toolExecutor: new FiberToolExecutor($toolbox), includeSources: true);
+
+        $messages = new MessageBag(
+            Message::ofUser('Compare Symfony and Laravel.'),
+        );
+
+        $result = $agent->call($messages)->getResult();
+
+        $this->assertInstanceOf(TextResult::class, $result);
+        $this->assertSame(2, $invocationCount);
+
+        $metadata = $result->getMetadata();
+        $this->assertTrue($metadata->has('sources'));
+        $this->assertInstanceOf(SourceCollection::class, $sources = $metadata->get('sources'));
+        $this->assertCount(2, $sources);
+    }
+
+    public function testAgentAggregatesTokenUsageWithFiberExecutor()
+    {
+        $wikipedia = new Wikipedia(new MockHttpClient([
+            new JsonMockResponse([
+                'query' => [
+                    'search' => [['title' => 'PHP']],
+                ],
+            ]),
+        ]));
+
+        $toolbox = new Toolbox([$wikipedia]);
+
+        $invocationCount = 0;
+        $platform = new InMemoryPlatform(static function ($model, $input, $options) use (&$invocationCount) {
+            ++$invocationCount;
+
+            if (1 === $invocationCount) {
+                $result = new ToolCallResult([new ToolCall('call_php', 'wikipedia_search', ['query' => 'PHP programming'])]);
+                $result->getMetadata()->add('token_usage', new TokenUsage(promptTokens: 100, completionTokens: 20, totalTokens: 120));
+
+                return $result;
+            }
+
+            $result = new TextResult('PHP is a server-side scripting language.');
+            $result->getMetadata()->add('token_usage', new TokenUsage(promptTokens: 150, completionTokens: 30, totalTokens: 180));
+
+            return $result;
+        });
+
+        $agent = new Agent($platform, 'gpt-4', toolbox: $toolbox, toolExecutor: new FiberToolExecutor($toolbox));
+
+        $messages = new MessageBag(
+            Message::ofUser('Tell me about PHP.'),
+        );
+
+        $result = $agent->call($messages)->getResult();
+
+        $this->assertInstanceOf(TextResult::class, $result);
+        $this->assertSame(2, $invocationCount);
+        // The agent aggregates the token usage of every tool calling round with the final one.
+        $tokenUsage = $result->getMetadata()->get('token_usage');
+        $this->assertInstanceOf(TokenUsageAggregation::class, $tokenUsage);
+        $this->assertSame(2, $tokenUsage->count());
+        $this->assertSame(120 + 180, $tokenUsage->getTotalTokens());
+    }
+}